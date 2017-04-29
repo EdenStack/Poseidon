09e88c743a692ee01e64fbc2c8c3993f75b7cfe6
Tneciv <tnecivl@gmail.com>
2017-04-29 20:36:51 +0800

    remove angular material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,7 +8,8 @@ import {JournalComponent} from './journal/journal.component';
 import {JournalService} from "./journal/journal.service";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {HttpService} from "./common/http.service";
-import {MdButtonModule, MdCardModule, MdCheckboxModule, MdRadioModule} from "@angular/material";
+import {RouterModule} from '@angular/router';
+import {LUOO_APP_ROUTERS} from './common/routers';
 
 @NgModule({
   declarations: [
@@ -19,11 +20,8 @@ import {MdButtonModule, MdCardModule, MdCheckboxModule, MdRadioModule} from "@an
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot(),
-    MdButtonModule,
-    MdCheckboxModule,
-    MdRadioModule,
-    MdCardModule
+    RouterModule.forRoot(LUOO_APP_ROUTERS),
+    NgbModule.forRoot()
   ],
   providers: [JournalService, HttpService],
   bootstrap: [AppComponent]
