fd76e5b639a1f36150d52d57f8f40f896861d48a
Tneciv <tnecivl@gmail.com>
2017-05-21 15:25:10 +0800

    handle crawler info

--- crawler/crawler/spiders/luoospider.py
+++ crawler/crawler/spiders/luoospider.py
@@ -11,6 +11,7 @@ class LuooCrawler(scrapy.Spider):
     name = 'luoo'
     htmlParser = 'html.parser'
     href = 'href'
+    isDebug = True
 
     def start_requests(self):
         urls = ['http://www.luoo.net/music']
@@ -18,12 +19,14 @@ class LuooCrawler(scrapy.Spider):
             yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
+        
         # 请求返回的html源码，从首页开始，然后处理分页
         content = response.body
         if not content:
             self.log('parse body error.')
             return
         soup = BeautifulSoup(response.body, self.htmlParser)
+        
         # 获取当前页面所有期刊url（这时候处理的还是首页 http://www.luoo.net/music 的）
         journalUrlList = []
         for nextPageUrl in soup.select('.vol-list .item'):
@@ -35,8 +38,12 @@ class LuooCrawler(scrapy.Spider):
         # 开始处理当前页面所有期刊
         for journalUrl in journalUrlList:
             yield scrapy.Request(journalUrl, self.parse_page)
+
+        if (self.isDebug):
+            return
+
         ## 处理完当前页开始找下一页地址，递归
-        
+
         # 开始处理分页
         nextPageUrl = soup.select('.next')[0].get(self.href)
 
@@ -52,12 +59,12 @@ class LuooCrawler(scrapy.Spider):
     def parse_page(self, response):
         soup = BeautifulSoup(response.body, self.htmlParser)
         luooItem = LuooItem()
-        trackItem = TrackItem()
+        trackList = []
         cover = soup.select('.vol-cover-wrapper')
+        tracksSoup = soup.select('.vol-tracklist')[0]
 
         if cover:
-            prefix = '.jpg'
-            coverImg = cover[0].img.get('src').split(prefix)[0] + prefix
+            coverImg = self.handleImgUrl(cover[0].img.get('src'))
             luooItem['volImg'] = coverImg
 
         luooItem['volDesc'] = soup.select('.vol-desc')[0].text
@@ -66,4 +73,24 @@ class LuooCrawler(scrapy.Spider):
         luooItem['volNumber'] = soup.select('.vol-number')[0].text
         luooItem['volDate'] = soup.select('.vol-date')[0].text
 
+        for track in tracksSoup.select('.track-item'):
+            item = self.handleItem(track)
+            trackList.append(item)
+
+        luooItem['volTracks'] = trackList
         self.log(luooItem)
+
+    def handleItem(self, trackInfo):
+        trackItem = TrackItem()
+        self.log(trackInfo)
+        trackItem['trackName'] = trackInfo.select('.trackname')[0].text
+        trackItem['trackAlbum'] = trackInfo.select('.album')[0].text
+        trackItem['trackArtist'] = trackInfo.select('.artist')[0].text
+        trackItem['trackImg'] = trackInfo.select('.cover')[0]
+        trackItem['trackMP3Url'] = 'xxxxx.mp3'
+        return trackItem
+
+    def handleImgUrl(self, imgSrc):
+        prefix = '.jpg'
+        coverImg = imgSrc.split(prefix)[0] + prefix
+        return coverImg
